d919b589491cf45980d6de801240218e5e2789e6
hcguler <cihad_gulerhotmail.com>
2020-03-19 09:39:19 +0300

    spring security ile basic auth eklendi ve csrf disable edildi.

--- src/main/java/com/hcg/mastermicroservices/user/configuration/MasterMicroservicesConfig.java
+++ src/main/java/com/hcg/mastermicroservices/user/configuration/MasterMicroservicesConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 /**
  * @author hcguler
  */
@@ -13,5 +14,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.hcg.mastermicroservices.user.*"})
 @EnableJpaRepositories(basePackages = {"com.hcg.mastermicroservices.user.*"})
 public class MasterMicroservicesConfig {
-
 }
